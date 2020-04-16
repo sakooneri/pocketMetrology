886d6ae95bc78e8101df646144e9b2731f4de290
sakooneri <savis2212@gmail.com>
2020-04-16 21:58:34 +0300

    Revert "разделил на несколько файлов, создал мейкфайл"
    This reverts commit ddbcebea228cc09ce28d47d94b595b767e683f8f.

--- formulas.h
+++
@@ -1,7 +0,0 @@
-void Absolute_Error(unsigned short int choice_parameter);
-
-void Reletive_Error(unsigned short int choice_parameter);
-
-void Reduced_Error(unsigned short int choice_parameter);
-
-void Sensivity(unsigned short int choice_parameter);
